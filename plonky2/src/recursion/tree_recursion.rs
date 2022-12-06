e5cce20e67474f98b3af51565caf50c1e34ad612
Sai Deng <dengsai@hotmail.com>
2022-12-06 08:53:34 -0800

    added unit tests for zk-tree circuits

--- plonky2/src/recursion/tree_recursion.rs
+++ plonky2/src/recursion/tree_recursion.rs
@@ -17,15 +17,28 @@ use crate::plonk::circuit_data::{
 use crate::plonk::config::{AlgebraicHasher, GenericConfig};
 use crate::plonk::proof::{ProofTarget, ProofWithPublicInputs, ProofWithPublicInputsTarget};
 
-pub struct TreeRecursionData<
+pub struct TreeRecursionNodeData<
     'a,
     F: RichField + Extendable<D>,
     C: GenericConfig<D, F = F>,
     const D: usize,
 > {
-    proof: &'a ProofWithPublicInputs<F, C, D>,
+    proof0: &'a ProofWithPublicInputs<F, C, D>,
+    proof1: &'a ProofWithPublicInputs<F, C, D>,
+    verifier_data0: &'a VerifierOnlyCircuitData<C, D>,
+    verifier_data1: &'a VerifierOnlyCircuitData<C, D>,
     verifier_data: &'a VerifierOnlyCircuitData<C, D>,
-    common_data: &'a CommonCircuitData<F, D>,
+}
+
+pub struct TreeRecursionLeafData<
+    'a,
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+> {
+    inner_proof: &'a ProofWithPublicInputs<F, C, D>,
+    inner_verifier_data: &'a VerifierOnlyCircuitData<C, D>,
+    verifier_data: &'a VerifierOnlyCircuitData<C, D>,
 }
 
 pub struct TreeRecursionNodeTarget<const D: usize> {
@@ -33,6 +46,7 @@ pub struct TreeRecursionNodeTarget<const D: usize> {
     pub proof1: ProofWithPublicInputsTarget<D>,
     pub verifier_data0: VerifierCircuitTarget,
     pub verifier_data1: VerifierCircuitTarget,
+    pub verifier_data: VerifierCircuitTarget,
 }
 
 pub struct TreeRecursionLeafTarget<const D: usize> {
@@ -103,7 +117,7 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
         assert!(self.verifier_data_public_input.is_none());
         self.add_verifier_data_public_input();
         let verifier_data = self.verifier_data_public_input.clone().unwrap();
-        assert_eq!(common_data.num_public_inputs, self.num_public_inputs());
+        common_data.num_public_inputs = self.num_public_inputs();
 
         let proof0 = self.add_virtual_proof_with_pis::<C>(common_data);
         let proof1 = self.add_virtual_proof_with_pis::<C>(common_data);
@@ -152,6 +166,7 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
             proof1,
             verifier_data0,
             verifier_data1,
+            verifier_data,
         })
     }
 
@@ -176,7 +191,7 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
         assert!(self.verifier_data_public_input.is_none());
         self.add_verifier_data_public_input();
         let verifier_data = self.verifier_data_public_input.clone().unwrap();
-        assert_eq!(common_data.num_public_inputs, self.num_public_inputs());
+        common_data.num_public_inputs = self.num_public_inputs();
 
         let inner_proof = self.add_virtual_proof_with_pis::<C>(&inner_common_data);
         let inner_verifier_data = VerifierCircuitTarget {
@@ -189,8 +204,8 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
         self.connect_hashes(inputs_hash, h);
         let h = self.hash_n_to_hash_no_pad::<C::Hasher>(
             [
-                verifier_data.circuit_digest.elements,
                 inner_verifier_data.circuit_digest.elements,
+                verifier_data.circuit_digest.elements,
             ]
             .concat(),
         );
@@ -219,7 +234,7 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
     }
 }
 
-/// Set the targets in a `TreeRecursionTarget` to their corresponding values in a `TreeRecursionData`.
+/// Set the targets in a `TreeRecursionNodeTarget` to their corresponding values in a `TreeRecursionNodeData`.
 pub fn set_tree_recursion_node_data_target<
     F: RichField + Extendable<D>,
     C: GenericConfig<D, F = F>,
@@ -227,27 +242,59 @@ pub fn set_tree_recursion_node_data_target<
 >(
     pw: &mut PartialWitness<F>,
     tree_recursion_data_target: &TreeRecursionNodeTarget<D>,
-    tree_recursion_data0: &TreeRecursionData<F, C, D>,
-    tree_recursion_data1: &TreeRecursionData<F, C, D>,
+    tree_recursion_data: &TreeRecursionNodeData<F, C, D>,
 ) -> Result<()>
 where
     C::Hasher: AlgebraicHasher<F>,
 {
     pw.set_proof_with_pis_target(
         &tree_recursion_data_target.proof0,
-        tree_recursion_data0.proof,
+        tree_recursion_data.proof0,
     );
     pw.set_proof_with_pis_target(
         &tree_recursion_data_target.proof1,
-        tree_recursion_data1.proof,
+        tree_recursion_data.proof1,
     );
     pw.set_verifier_data_target(
         &tree_recursion_data_target.verifier_data0,
-        tree_recursion_data0.verifier_data,
+        tree_recursion_data.verifier_data0,
     );
     pw.set_verifier_data_target(
         &tree_recursion_data_target.verifier_data1,
-        tree_recursion_data1.verifier_data,
+        tree_recursion_data.verifier_data1,
+    );
+    pw.set_verifier_data_target(
+        &tree_recursion_data_target.verifier_data,
+        tree_recursion_data.verifier_data,
+    );
+
+    Ok(())
+}
+
+/// Set the targets in a `TreeRecursionLeafTarget` to their corresponding values in a `TreeRecursionLeafData`.
+pub fn set_tree_recursion_leaf_data_target<
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+>(
+    pw: &mut PartialWitness<F>,
+    tree_recursion_data_target: &TreeRecursionLeafTarget<D>,
+    tree_recursion_data: &TreeRecursionLeafData<F, C, D>,
+) -> Result<()>
+where
+    C::Hasher: AlgebraicHasher<F>,
+{
+    pw.set_proof_with_pis_target(
+        &tree_recursion_data_target.inner_proof,
+        tree_recursion_data.inner_proof,
+    );
+    pw.set_verifier_data_target(
+        &tree_recursion_data_target.inner_verifier_data,
+        tree_recursion_data.inner_verifier_data,
+    );
+    pw.set_verifier_data_target(
+        &tree_recursion_data_target.verifier_data,
+        tree_recursion_data.verifier_data,
     );
 
     Ok(())
@@ -277,19 +324,21 @@ where
 #[cfg(test)]
 mod tests {
     use anyhow::Result;
-    use plonky2::plonk::circuit_data::VerifierOnlyCircuitData;
-    use plonky2::plonk::proof::ProofWithPublicInputs;
 
     use crate::field::extension::Extendable;
-    use crate::field::types::{Field, PrimeField64};
+    use crate::field::types::Field;
     use crate::gates::noop::NoopGate;
     use crate::hash::hash_types::{HashOut, RichField};
     use crate::hash::hashing::hash_n_to_hash_no_pad;
-    use crate::hash::poseidon::{PoseidonHash, PoseidonPermutation};
+    use crate::hash::poseidon::PoseidonPermutation;
     use crate::iop::witness::{PartialWitness, Witness};
     use crate::plonk::circuit_builder::CircuitBuilder;
     use crate::plonk::circuit_data::{CircuitConfig, CommonCircuitData, VerifierCircuitTarget};
     use crate::plonk::config::{AlgebraicHasher, GenericConfig, PoseidonGoldilocksConfig};
+    use crate::recursion::tree_recursion::{
+        check_tree_proof_verifier_data, set_tree_recursion_leaf_data_target,
+        set_tree_recursion_node_data_target, TreeRecursionLeafData, TreeRecursionNodeData,
+    };
 
     // Generates `CommonCircuitData` usable for recursion.
     fn common_data_for_recursion<
@@ -335,10 +384,10 @@ mod tests {
 
         let config = CircuitConfig::standard_recursion_config();
 
-        let hash0 = HashOut {
-            elements: [F::ZERO, F::ONE, F::TWO, F::from_canonical_usize(3)],
-        };
         // create dummy proof0
+        let hash0 = HashOut {
+            elements: [F::ZERO, F::ZERO, F::ZERO, F::ZERO],
+        };
         let mut builder = CircuitBuilder::<F, D>::new(config.clone());
         for _ in 0..1_000 {
             builder.add_gate(NoopGate, vec![]);
@@ -348,122 +397,125 @@ mod tests {
         let data = builder.build::<C>();
         let mut inputs = PartialWitness::new();
         inputs.set_hash_target(input_hash, hash0);
-        let proof = data.prove(inputs)?;
-        data.verify(proof.clone())?;
+        let proof0 = data.prove(inputs)?;
+        data.verify(proof0.clone())?;
+        let cd0 = data.common;
+        let vd0 = data.verifier_only;
 
-        //
-        // // Circuit that computes a repeated hash.
-        // let initial_hash = builder.add_virtual_hash();
-        // builder.register_public_inputs(&initial_hash.elements);
-        // // Hash from the previous proof.
-        // let old_hash = builder.add_virtual_hash();
-        // // The input hash is either the previous hash or the initial hash depending on whether
-        // // the last proof was a base case.
-        // let input_hash = builder.add_virtual_hash();
-        // let h = builder.hash_n_to_hash_no_pad::<PoseidonHash>(input_hash.elements.to_vec());
-        // builder.register_public_inputs(&h.elements);
-        // // Previous counter.
-        // let old_counter = builder.add_virtual_target();
-        // let new_counter = builder.add_virtual_public_input();
-        // let old_pis = [
-        //     initial_hash.elements.as_slice(),
-        //     old_hash.elements.as_slice(),
-        //     [old_counter].as_slice(),
-        // ]
-        //     .concat();
-        //
-        // let mut common_data = common_data_for_recursion::<F, C, D>();
-        //
-        // let condition = builder.add_virtual_bool_target_safe();
-        // // Add tree recursion gadget.
-        // let tree_data_target =
-        //     builder.tree_recursion::<C>(condition, &old_pis, &mut common_data)?;
-        // let input_hash_bis =
-        //     builder.select_hash(tree_data_target.condition, old_hash, initial_hash);
-        // builder.connect_hashes(input_hash, input_hash_bis);
-        // // New counter is the previous counter +1 if the previous proof wasn't a base case.
-        // let new_counter_bis = builder.add(old_counter, condition.target);
-        // builder.connect(new_counter, new_counter_bis);
-        //
-        // let tree_circuit_data = builder.build::<C>();
-        //
-        // let tree_recursion_data = TreeRecursionData {
-        //     proof: &None, // Base case: We don't have a proof to put here yet.
-        //     verifier_data: &tree_circuit_data.verifier_only,
-        //     common_data: &tree_circuit_data.common,
-        // };
-        // let initial_hash = [F::ZERO, F::ONE, F::TWO, F::from_canonical_usize(3)];
-        // set_tree_recursion_data_target(
-        //     &mut pw,
-        //     &tree_data_target,
-        //     &tree_recursion_data,
-        //     &initial_hash,
-        // )?;
-        // let proof = tree_circuit_data.prove(pw)?;
-        // check_tree_proof_verifier_data(
-        //     &proof,
-        //     tree_recursion_data.verifier_data,
-        //     tree_recursion_data.common_data,
-        // )?;
-        // tree_circuit_data.verify(proof.clone())?;
-        //
-        // // 1st recursive layer.
-        // let mut pw = PartialWitness::new();
-        // let tree_recursion_data = TreeRecursionData {
-        //     proof: &Some(proof), // Input previous proof.
-        //     verifier_data: &tree_circuit_data.verifier_only,
-        //     common_data: &tree_circuit_data.common,
-        // };
-        // set_tree_recursion_data_target(
-        //     &mut pw,
-        //     &tree_data_target,
-        //     &tree_recursion_data,
-        //     &[],
-        // )?;
-        // let proof = tree_circuit_data.prove(pw)?;
-        // check_tree_proof_verifier_data(
-        //     &proof,
-        //     tree_recursion_data.verifier_data,
-        //     tree_recursion_data.common_data,
-        // )?;
-        // tree_circuit_data.verify(proof.clone())?;
-        //
-        // // 2nd recursive layer.
-        // let mut pw = PartialWitness::new();
-        // let tree_recursion_data = TreeRecursionData {
-        //     proof: &Some(proof), // Input previous proof.
-        //     verifier_data: &tree_circuit_data.verifier_only,
-        //     common_data: &tree_circuit_data.common,
-        // };
-        // set_tree_recursion_data_target(
-        //     &mut pw,
-        //     &tree_data_target,
-        //     &tree_recursion_data,
-        //     &[],
-        // )?;
-        // let proof = tree_circuit_data.prove(pw)?;
-        // check_tree_proof_verifier_data(
-        //     &proof,
-        //     tree_recursion_data.verifier_data,
-        //     tree_recursion_data.common_data,
-        // )?;
-        //
-        // // Verify that the proof correctly computes a repeated hash.
-        // let initial_hash = &proof.public_inputs[..4];
-        // let hash = &proof.public_inputs[4..8];
-        // let counter = proof.public_inputs[8];
-        // let mut h: [F; 4] = initial_hash.try_into().unwrap();
-        // assert_eq!(
-        //     hash,
-        //     core::iter::repeat_with(|| {
-        //         h = hash_n_to_hash_no_pad::<F, PoseidonPermutation>(&h).elements;
-        //         h
-        //     })
-        //         .nth(counter.to_canonical_u64() as usize)
-        //         .unwrap()
-        // );
-        //
-        // tree_circuit_data.verify(proof)
+        // create dummy proof1
+        let hash1 = HashOut {
+            elements: [F::ZERO, F::ZERO, F::ZERO, F::ONE],
+        };
+        let mut builder = CircuitBuilder::<F, D>::new(config.clone());
+        for _ in 0..2_000 {
+            builder.add_gate(NoopGate, vec![]);
+        }
+        let input_hash = builder.add_virtual_hash();
+        builder.register_public_inputs(&input_hash.elements);
+        let data = builder.build::<C>();
+        let mut inputs = PartialWitness::new();
+        inputs.set_hash_target(input_hash, hash1);
+        let proof1 = data.prove(inputs)?;
+        data.verify(proof1.clone())?;
+        let cd1 = data.common;
+        let vd1 = data.verifier_only;
+
+        let mut common_data = common_data_for_recursion::<F, C, D>();
+        // build leaf0
+        let mut builder = CircuitBuilder::<F, D>::new(config.clone());
+        let leaf_targets = builder.tree_recursion_leaf::<C>(cd0, &mut common_data)?;
+        let data = builder.build::<C>();
+        let leaf_vd0 = &data.verifier_only;
+        let mut pw = PartialWitness::new();
+        let leaf_data = TreeRecursionLeafData {
+            inner_proof: &proof0,
+            inner_verifier_data: &vd0,
+            verifier_data: leaf_vd0,
+        };
+        set_tree_recursion_leaf_data_target(&mut pw, &leaf_targets, &leaf_data)?;
+        let leaf_proof0 = data.prove(pw)?;
+        check_tree_proof_verifier_data(&leaf_proof0, leaf_vd0, &common_data)
+            .expect("Leaf 0 public inputs do not match its verifier data");
+
+        // build leaf1
+        let mut builder = CircuitBuilder::<F, D>::new(config.clone());
+        let leaf_targets = builder.tree_recursion_leaf::<C>(cd1, &mut common_data)?;
+        let data = builder.build::<C>();
+        let leaf_vd1 = &data.verifier_only;
+        let mut pw = PartialWitness::new();
+        let leaf_data = TreeRecursionLeafData {
+            inner_proof: &proof1,
+            inner_verifier_data: &vd1,
+            verifier_data: leaf_vd1,
+        };
+        set_tree_recursion_leaf_data_target(&mut pw, &leaf_targets, &leaf_data)?;
+        let leaf_proof1 = data.prove(pw)?;
+        check_tree_proof_verifier_data(&leaf_proof1, leaf_vd1, &common_data)
+            .expect("Leaf 1 public inputs do not match its verifier data");
+
+        // build node
+        let mut builder = CircuitBuilder::<F, D>::new(config.clone());
+        let node_targets = builder.tree_recursion_node::<C>(&mut common_data)?;
+        let data = builder.build::<C>();
+        let node_vd = &data.verifier_only;
+        let mut pw = PartialWitness::new();
+        let node_data = TreeRecursionNodeData {
+            proof0: &leaf_proof0,
+            proof1: &leaf_proof1,
+            verifier_data0: &leaf_vd0,
+            verifier_data1: &leaf_vd1,
+            verifier_data: node_vd,
+        };
+        set_tree_recursion_node_data_target(&mut pw, &node_targets, &node_data)?;
+        let node_proof = data.prove(pw)?;
+        check_tree_proof_verifier_data(&node_proof, node_vd, &common_data)
+            .expect("Node public inputs do not match its verifier data");
+
+        // Verify that the proof correctly computes the input hash.
+        let leaf0_input_hash = hash_n_to_hash_no_pad::<F, PoseidonPermutation>(&hash0.elements);
+        let leaf1_input_hash = hash_n_to_hash_no_pad::<F, PoseidonPermutation>(&hash1.elements);
+        assert_eq!(leaf0_input_hash.elements, leaf_proof0.public_inputs[0..4]);
+        assert_eq!(leaf1_input_hash.elements, leaf_proof1.public_inputs[0..4]);
+        let node_input_hash = hash_n_to_hash_no_pad::<F, PoseidonPermutation>(
+            [
+                leaf0_input_hash.elements.to_vec(),
+                leaf1_input_hash.elements.to_vec(),
+            ]
+            .concat()
+            .as_slice(),
+        );
+        assert_eq!(node_input_hash.elements, node_proof.public_inputs[0..4]);
+
+        // Verify that the proof correctly computes the circuit hash.
+        let leaf0_circuit_hash = hash_n_to_hash_no_pad::<F, PoseidonPermutation>(
+            [
+                vd0.circuit_digest.elements.to_vec(),
+                leaf_vd0.circuit_digest.elements.to_vec(),
+            ]
+            .concat()
+            .as_slice(),
+        );
+        assert_eq!(leaf0_circuit_hash.elements, leaf_proof0.public_inputs[4..8]);
+        let leaf1_circuit_hash = hash_n_to_hash_no_pad::<F, PoseidonPermutation>(
+            [
+                vd1.circuit_digest.elements.to_vec(),
+                leaf_vd1.circuit_digest.elements.to_vec(),
+            ]
+            .concat()
+            .as_slice(),
+        );
+        assert_eq!(leaf1_circuit_hash.elements, leaf_proof1.public_inputs[4..8]);
+
+        let node_circuit_hash = hash_n_to_hash_no_pad::<F, PoseidonPermutation>(
+            [
+                leaf0_circuit_hash.elements.to_vec(),
+                node_vd.circuit_digest.elements.to_vec(),
+                leaf1_circuit_hash.elements.to_vec(),
+            ]
+            .concat()
+            .as_slice(),
+        );
+        assert_eq!(node_circuit_hash.elements, node_proof.public_inputs[4..8]);
 
         Ok(())
     }
